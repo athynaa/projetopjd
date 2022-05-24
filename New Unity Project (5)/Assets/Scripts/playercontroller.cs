a612e91fc6d0be3a2670efbe8d69b4c380a59e75
Áthyna Cecília Ribeiro Silva <athyna.ribeiro@academico.ifrn.edu.br>
2022-05-24 14:54:25 -0300

    prog

---
+++ New Unity Project (5)/Assets/Scripts/playercontroller.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playercontroller : MonoBehaviour
+{
+   private Controles _controles;
+   private PlayerInput _playerInput;
+   private Camera _mainCamera;
+   private Vector2 _moveInput;
+   private Rigidbody _rigidbody;
+   public float moveMultiplier;
+
+   private void OnEnable()
+   {
+      _rigidbody = GetComponent<Rigidbody>();
+      
+      _controles = new Controles();
+      
+      _playerInput = GetComponent<PlayerInput>();
+      
+      _mainCamera = Camera.main;
+      
+      _playerInput.onActionTriggered += OnActionTriggered;
+         
+   }
+
+   private void OnDisable()
+   {
+      _playerInput.onActionTriggered -= OnActionTriggered;
+   }
+
+   private void OnActionTriggered(InputAction.CallbackContext obj)
+   {
+      if (obj.action.name.CompareTo(_controles.gameplay.move.name) != 0)
+      {
+         _moveInput = obj.ReadValue<Vector2>();
+      }
+     
+   }
+
+   private void Move()
+   {
+      _rigidbody.AddForce((_mainCamera.transform.forward * _moveInput.y +
+                          _mainCamera.transform.right * _moveInput.x ) * 
+                           moveMultiplier * Time.fixedDeltaTime);
+   }
+
+   private void FixedUpdate()
+   {
+      Move();
+   }
+
+}
---
+++ New Unity Project (5)/Assets/input/controles.cs
@@ -0,0 +1,252 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/input/Controles.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controles : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controles()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controles"",
+    ""maps"": [
+        {
+            ""name"": ""gameplay"",
+            ""id"": ""dd9de302-6f53-4589-be5c-7a6341d9de3e"",
+            ""actions"": [
+                {
+                    ""name"": ""move"",
+                    ""type"": ""Value"",
+                    ""id"": ""725a82fc-7124-449c-a876-780cf3e61e0e"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""arrows"",
+                    ""id"": ""afe6ae94-e671-4d4a-9747-ddeacf30b613"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""d2f5a193-beb3-45e8-a441-ddcab5b63e60"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""85e99576-c66f-4fc0-9f53-969543775bfc"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""08b7e779-4fdd-4bdb-99ac-62d5137c593d"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""cb5be0ee-8e4b-4401-afbf-44587f4f5e76"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""b8b57533-9f22-469b-b152-bd606004d8cc"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""d27d38fd-1731-45b5-9bc6-a2113bab5d3a"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""ae2aaca7-8d6b-4f18-9a27-45fc29c81c20"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""8a0cdc4d-9e12-4d19-a0af-0383dce99671"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""ab16d25c-bb13-4ce9-8efb-20c67328981a"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""keyboard"",
+            ""bindingGroup"": ""keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // gameplay
+        m_gameplay = asset.FindActionMap("gameplay", throwIfNotFound: true);
+        m_gameplay_move = m_gameplay.FindAction("move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // gameplay
+    private readonly InputActionMap m_gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_gameplay_move;
+    public struct GameplayActions
+    {
+        private @Controles m_Wrapper;
+        public GameplayActions(@Controles wrapper) { m_Wrapper = wrapper; }
+        public InputAction @move => m_Wrapper.m_gameplay_move;
+        public InputActionMap Get() { return m_Wrapper.m_gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @move.started += instance.OnMove;
+                @move.performed += instance.OnMove;
+                @move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public GameplayActions @gameplay => new GameplayActions(this);
+    private int m_keyboardSchemeIndex = -1;
+    public InputControlScheme keyboardScheme
+    {
+        get
+        {
+            if (m_keyboardSchemeIndex == -1) m_keyboardSchemeIndex = asset.FindControlSchemeIndex("keyboard");
+            return asset.controlSchemes[m_keyboardSchemeIndex];
+        }
+    }
+    public interface IGameplayActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
